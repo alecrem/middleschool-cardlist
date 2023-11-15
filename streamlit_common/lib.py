dfa94a957af52d372f0daac8022f1c6035c4ecfd
Alejandro Cremades <ale@alecrem.com>
2023-11-15 18:43:10 +0900

    Document the functions

--- streamlit_common/lib.py
+++ streamlit_common/lib.py
@@ -4,18 +4,23 @@ import re
 import urllib.parse
 
 
-def compose_scryfall_url(x: str) -> str:
-    return f"https://scryfall.com/search?q=prefer%3Aoldest%20!%22{urllib.parse.quote_plus(x)}%22"
+def compose_scryfall_url(cardname: str) -> str:
+    """Compose a Scryfall URL from the passed card name"""
+    return f"https://scryfall.com/search?q=prefer%3Aoldest%20!%22{urllib.parse.quote_plus(cardname)}%22"
 
 
-def row_to_link(x: pd.DataFrame) -> None:
-    cardname = x["name"]
-    if x.name_ja is not "":
-        cardname = f"{cardname} / {x.name_ja}"
-    st.markdown(f"- [{cardname}]({x.link})")
+def row_to_link(row: pd.DataFrame) -> None:
+    """Prints a list item with a Scryfall link for the card in the row passed"""
+    cardname = row["name"]
+    if row.name_ja is not "":
+        cardname = f"{cardname} / {row.name_ja}"
+    st.markdown(f"- [{cardname}]({row.link})")
 
 
 def get_legal_cardnames(cardname: str, mslist_df: pd.DataFrame) -> list:
+    """Returns a list with the English and Japanese names for the card
+    if there is an exact match, or `None` if there is not.
+    """
     english_match = mslist_df[mslist_df["name"].str.lower() == cardname.lower()]
     cardname_en_list = None
     if english_match.shape[0] > 0:
@@ -34,6 +39,9 @@ def get_legal_cardnames(cardname: str, mslist_df: pd.DataFrame) -> list:
 
 
 def remove_number_of_copies(line: str) -> str:
+    """Remove the number of copies in front of the card name
+    from a line in a card list
+    """
     if len(line.strip()) < 1:
         return None
     pattern = re.compile("^([0-9]+) +")
@@ -41,6 +49,7 @@ def remove_number_of_copies(line: str) -> str:
 
 
 def is_cardname_legal(cardname: str, mslist_df: pd.DataFrame) -> bool:
+    """Returns wether a card with exactly the passed name is legal in the format"""
     if mslist_df[mslist_df["name"].str.lower() == cardname.lower()].shape[0] > 0:
         return True
     if mslist_df[mslist_df["name_ja"] == cardname].shape[0] > 0:
