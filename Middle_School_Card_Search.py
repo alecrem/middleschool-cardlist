59718b5c5d19de4f2d84ecc388bf56d927da4e6e
Alejandro Cremades <ale@alecrem.com>
2023-11-18 10:52:26 +0900

    Localize card search page

--- Middle_School_Card_Search.py
+++ Middle_School_Card_Search.py
@@ -2,28 +2,30 @@ import streamlit as st
 import pandas as pd
 import streamlit_common.footer
 import streamlit_common.lib as lib
+import streamlit_common.locale
 
 mslist_path = "output/middleschool.csv"
 number_shown_results = 20
+_ = streamlit_common.locale.get_locale()
 
 st.set_page_config(
     page_title="Middle School | Card Search",
     page_icon="🃏",
     layout="wide",
 )
-st.write(
-    """
-    # Middle School Card Search
-
-    Enter any English or Japanese text to find all Middle School legal card titles which include it.
-    """
+l = st.sidebar.radio(
+    label="luage / 言語",
+    options=["en", "ja"],
+    captions=["English", "日本語"],
 )
+st.write(f'# {_["search"]["title"][l]}')
+st.write(_["search"]["instructions"][l])
 
 mslist_df = pd.read_csv(mslist_path)
 mslist_df.fillna("", inplace=True)
-st.write(mslist_df.shape[0], "cards are legal")
+st.write(f'**{mslist_df.shape[0]}**{_["search"]["cards_are_legal"][l]}')
 
-name_input = st.text_input(f"Search by card name").strip()
+name_input = st.text_input(_["search"]["search_by_card_name"][l]).strip()
 exact_match = lib.get_legal_cardnames(name_input, mslist_df)
 results_en_df = mslist_df[
     mslist_df["name"].str.contains(name_input.lower(), case=False)
@@ -38,11 +40,11 @@ if name_input:
         if exact_match[2] is not None:
             cardname = f"{cardname} / {exact_match[2]}"
         st.write(
-            f"✅ [{cardname}]({lib.compose_scryfall_url(exact_match[1])}) is an exact match"
+            f'✅ [{cardname}]({lib.compose_scryfall_url(exact_match[1])}) {_["search"]["exact_match"][l]}'
         )
-    st.write(results_df.shape[0], f'cards found by "{name_input}"')
+    st.write(f'**{results_df.shape[0]}**{_["search"]["cards_found"][l]}')
     if results_df.shape[0] > number_shown_results:
-        st.write(f"Top {number_shown_results} results:")
+        st.write(_["search"]["top_results"][l])
     results_df["link"] = results_df["name"].apply(lib.compose_scryfall_url)
     results_df[:number_shown_results].transpose().apply(lib.row_to_link)
 
---
+++ streamlit_common/locale.py
@@ -0,0 +1,30 @@
+def get_locale():
+    return {
+        "search": {
+            "title": {"en": "Middle School Card Search", "ja": "ミドルスクール カード検索"},
+            "instructions": {
+                "en": "Enter any English or Japanese text to find all [Middle School legal](https://www.eternalcentral.com/middleschoolrules/) card titles which include it.",
+                "ja": "カードの英語名か日本語名を入力し始めると[ミドルスクールで使用可能](https://www.eternalcentral.com/middleschoolrules/)なカード名が引っかかります。",
+            },
+            "cards_are_legal": {
+                "en": " cards are legal.",
+                "ja": "枚 のカードが使用可能です。",
+            },
+            "search_by_card_name": {
+                "en": "Search by card name:",
+                "ja": "カード名で検索してください：",
+            },
+            "exact_match": {
+                "en": "is an exact match.",
+                "ja": "が完全一致します。",
+            },
+            "cards_found": {
+                "en": " cards were found.",
+                "ja": "枚 のカードが見つかりました。",
+            },
+            "top_results": {
+                "en": "Top results:",
+                "ja": "上位の検索結果：",
+            },
+        },
+    }
